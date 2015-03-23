eb4f7f813c40ca662365091bb681692e32e04a50
Tom Valk <tomvalk@gmail.com>
2015-03-23 15:57:29 +0100

    Control base class

--- src/Controls/Control.php
+++ src/Controls/Control.php
@@ -8,4 +8,42 @@ abstract class Control {
     private $id;
     private $class;
     private $value;
+    private $label;
+    
+    public function __construct($name, $id = null, $value = null) {
+        $this->name = $name;
+        $this->id = $id;
+        $this->value = $value;
+    }
+    
+    /**
+     * Get ID of control
+     * @return string|null
+     */
+    protected function getId() {
+        return $this->id;
+    }
+    
+    /**
+     * Get Name of control
+     * @return string
+     */
+    protected function getName() {
+        return $this->name;
+    }
+    
+    /**
+     * Get value of control
+     * @return mixed
+     */
+    protected function getValue() {
+        return $this->value;
+    }
+    
+    /**
+     * Output the control
+     */
+    public function __toString() {
+        \bootbuilder\BootBuilder::getForm()->renderControl($this);
+    }
 }
