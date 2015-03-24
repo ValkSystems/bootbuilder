edb77abe54616caff251a6eb32d0f8ca6aa4c402
Tom Valk <tomvalk@gmail.com>
2015-03-24 16:04:43 +0100

    Added password

--- src/bootbuilder/Controls/Password.php
+++ src/bootbuilder/Controls/Password.php
@@ -3,4 +3,14 @@ namespace bootbuilder\Controls;
 
 class Password extends Control {
     
+    /**
+     * Render basics of text field
+     */
+    public function renderBasic() {
+        $html = "<input type='password'";
+        $html .= " " . $this->getCompiledAttributes("form-control");
+        $html .= ">";
+        return $html;
+    }
+
 }
