1ba0db7d5a0c0afbefa40426cd3ad14c2e912f4e
Tom Valk <tomvalk@gmail.com>
2015-03-24 14:59:23 +0100

    Text input render added

--- src/bootbuilder/Controls/Text.php
+++ src/bootbuilder/Controls/Text.php
@@ -3,4 +3,18 @@ namespace bootbuilder\Controls;
 
 class Text extends Control {
     
+    /**
+     * Render basics of text field
+     */
+    public function renderBasic() {
+        $html = "<input type='text'";
+        if($this->id) $html .= " id='$this->id'";
+        if($this->class) $html .= " class='$this->class'";
+        if($this->name) $html .= " name='$this->name'";
+        if($this->placeholder) $html .= " placeholder='$this->placeholder'";
+        if($this->value) $html .= " value='$this->value'";
+        $html .= ">";
+        return $html;
+    }
+
 }
