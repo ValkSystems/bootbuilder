4d0c6502fd4e8699c4fed7e007caef48fc1664ce
Tom Valk <tomvalk@gmail.com>
2015-03-24 15:48:04 +0100

    Added TextArea

--- src/bootbuilder/Controls/TextArea.php
+++ src/bootbuilder/Controls/TextArea.php
@@ -2,12 +2,34 @@
 namespace bootbuilder\Controls;
 
 class TextArea extends Control {
+    private $rows = 3;
+    
+    /**
+     * Set number of rows, default is 3
+     * @param int $rows
+     */
+    public function setRows($rows) {
+        $this->rows = $rows;
+    }
+    
+    /**
+     * Get number of rows
+     * @return int
+     */
+    public function getRows() {
+        return $this->rows;
+    }
     
     /**
      * Render basics of textarea
      */
     public function renderBasic() {
-        
+        $html = "<textarea rows='$this->rows'";
+        $html .= " " . $this->getCompiledAttributes("form-control");
+        $html .= ">";
+        $html .= $this->value;
+        $html .= "</textarea>";
+        return $html;
     }
 
 }
--- src/bootbuilder/HorizontalForm.php
+++ src/bootbuilder/HorizontalForm.php
@@ -3,16 +3,16 @@
 namespace bootbuilder;
 
 class HorizontalForm extends Form {
-    private $labelcol = "col-md-3";
-    private $controlcol = "col-md-9";
+    private static $labelcol = "col-md-3";
+    private static $controlcol = "col-md-9";
     
     public function __construct($label_col = "col-md-3", $control_col = "col-md-9") {
         parent::__construct();
         
         $this->class = "form-horizontal";
         
-        $this->labelcol = $label_col;
-        $this->controlcol = $control_col;
+        self::$labelcol = $label_col;
+        self::$controlcol = $control_col;
     }
     
     public static function renderControl(\bootbuilder\Controls\Control $control, $return = false) {
@@ -20,10 +20,10 @@ class HorizontalForm extends Form {
         $html = "<div class='form-group'>";
         
         // Label
-        $html .= "<label for='{$control->getId()}' class='control-label $this->labelcol'>" . $control->getLabel() . "</label>";
+        $html .= "<label for='{$control->getId()}' class='control-label " . self::$labelcol . "'>" . $control->getLabel() . "</label>";
         
         // Control
-        $html .= "<div class='$this->controlcol'>";
+        $html .= "<div class='" . self::$controlcol . "'>";
         
         $html .= $control->renderBasic();
         
