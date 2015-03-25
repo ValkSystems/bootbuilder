5a214176ad1a3bf6022fa473898ea544228426ed
Tom Valk <tomvalk@gmail.com>
2015-03-25 11:21:44 +0100

    Radio added

--- src/bootbuilder/Controls/Radio.php
+++ src/bootbuilder/Controls/Radio.php
@@ -1,11 +1,17 @@
 <?php
 namespace bootbuilder\Controls;
 
-class Radio extends Control {
+class Radio extends Checkbox {
     
     
     public function renderBasic() {
-        
+        $html = "<div class='radio " . ($this->disabled ? "disabled" : "") . "'>";
+        $html .= "<label>";
+        $html .= "<input type='radio' " . ($this->checked === true ? "checked" : "") . " " . $this->getCompiledAttributes() . ">";
+        $html .= " " . $this->label;
+        $html .= "</label>";
+        $html .= "</div>";
+        return $html;
     }
 
 }
