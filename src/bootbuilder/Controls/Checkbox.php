4108cd76d7080e216f4d0bc6aa87653e5afcba58
Tom Valk <tomvalk@gmail.com>
2015-03-25 11:21:28 +0100

    Checked state added to checkbox

--- src/bootbuilder/Controls/Checkbox.php
+++ src/bootbuilder/Controls/Checkbox.php
@@ -2,12 +2,28 @@
 namespace bootbuilder\Controls;
 
 class Checkbox extends Control {
+    protected $checked = false;
     
+    /**
+     * Set Checked state
+     * @param boolean $checked
+     */
+    public function setChecked($checked) {
+        $this->checked = $checked;
+    }
+    
+    /**
+     * Get checked state
+     * @return boolean
+     */
+    public function isChecked() {
+        return $this->checked;
+    }
     
     public function renderBasic() {
         $html = "<div class='checkbox " . ($this->disabled ? "disabled" : "") . "'>";
         $html .= "<label>";
-        $html .= "<input type='checkbox' " . $this->getCompiledAttributes() . ">";
+        $html .= "<input type='checkbox' " . ($this->checked === true ? "checked" : "") . " " . $this->getCompiledAttributes() . ">";
         $html .= " " . $this->label;
         $html .= "</label>";
         $html .= "</div>";
