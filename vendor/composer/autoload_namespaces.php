f9746b19c11f0fd8cc85467ed7a7ae04a237d2e3
Tom Valk <tomvalk@gmail.com>
2015-05-01 10:13:08 +0200

    Composer update

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -8,6 +8,5 @@ $baseDir = dirname($vendorDir);
 return array(
     'phpDocumentor' => array($vendorDir . '/phpdocumentor/reflection-docblock/src'),
     'bootbuilder' => array($baseDir . '/src'),
-    'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
     'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
 );
